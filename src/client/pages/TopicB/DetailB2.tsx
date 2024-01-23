c391d87a5b5b658b212136d3bb729a9c611b552c
Mark Teets <markteets@gmail.com>
2024-01-23 16:13:11 -0700

    Sample detail pages

---
+++ src/client/pages/TopicB/DetailB2.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const DetailB2 = () => {
+  return (
+    <>
+      <h2>Title B2</h2>
+      <p>
+        Et qui magna nulla proident dolore. Id aliquip ea consectetur quis sit nisi id aute. Tempor
+        nisi amet incididunt anim consectetur tempor occaecat adipisicing id irure exercitation eu.
+      </p>
+      <p>
+        Sint in ipsum do laborum aliqua minim qui consectetur magna qui est ipsum in dolor. Lorem et
+        id proident laborum velit. Cupidatat ad tempor occaecat eiusmod elit aliquip ut. Do
+        excepteur laboris enim et ut amet occaecat excepteur excepteur est ullamco et. Deserunt
+        ipsum sint duis elit sint ipsum cupidatat sit. Cillum laborum velit est incididunt et do do
+        occaecat incididunt consectetur Lorem eu ea esse.
+      </p>
+      <p>
+        Occaecat eu fugiat deserunt Lorem pariatur reprehenderit. Fugiat magna aute cupidatat amet
+        consequat dolor do ea ea. Fugiat aliquip cupidatat fugiat aliquip ea veniam aliquip eu
+        commodo in aute dolore quis. Adipisicing ea pariatur eu anim sunt. Proident quis sunt do
+        duis duis nulla magna aliqua magna cupidatat ut commodo aute. Pariatur veniam irure tempor
+        excepteur exercitation ut officia culpa incididunt. Lorem sunt proident est commodo sint
+        commodo cillum incididunt reprehenderit.
+      </p>
+    </>
+  );
+};
+
+export default DetailB2;
